53bfb6dac75d0060c69072f548d29e93ce562272
rylp <rohan975357@gmail.com>
2020-10-27 21:47:27 +0530

    Minor Changes in Storing UserInfo. Add basic buttons in Main2

---
+++ app/src/main/java/com/example/raternet_isp_app/CurrentUser.java
@@ -0,0 +1,8 @@
+package com.example.raternet_isp_app;
+
+public class CurrentUser
+{
+    public static String USER_EMAIL="";
+    public static String USER_NAME="";
+    public static String USER_PHNO="";
+}

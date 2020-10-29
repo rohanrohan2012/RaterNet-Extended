8663e08c92e59512602fd8fd43c58480ed16986e
rylp <rohan975357@gmail.com>
2020-10-29 11:10:43 +0530

    Created 3 activities

---
+++ app/src/main/java/com/example/raternet_isp_app/IspDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.raternet_isp_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IspDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_isp_details);
+    }
+}
---
+++ app/src/main/java/com/example/raternet_isp_app/ReviewDetails.java
@@ -0,0 +1,4 @@
+package com.example.raternet_isp_app;
+
+public class ReviewDetails {
+}

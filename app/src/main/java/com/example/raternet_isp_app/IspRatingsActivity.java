ec8dc12ae132965c3b5f522fa0ece0e696ded949
rylp <rohan975357@gmail.com>
2020-10-30 14:23:48 +0530

    Finished Review Design + Minor changes

--- app/src/main/java/com/example/raternet_isp_app/IspRatingsActivity.java
+++ app/src/main/java/com/example/raternet_isp_app/IspRatingsActivity.java
@@ -3,12 +3,30 @@ package com.example.raternet_isp_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class IspRatingsActivity extends AppCompatActivity {
+
+    public RadioGroup RdoGrpType;
+    public RadioButton RdoBtnType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_isp_ratings);
+
+        RdoGrpType=findViewById(R.id.RdoGrpType);
+    }
+
+    public void checkType(View v)
+    {
+        int radioId=RdoGrpType.getCheckedRadioButtonId();
+
+        RdoBtnType=findViewById(radioId);
+
+        Toast.makeText(this, "Selected "+RdoBtnType.getText(), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/raternet_isp_app/MainActivity2.java
+++ app/src/main/java/com/example/raternet_isp_app/MainActivity2.java
@@ -23,10 +23,15 @@ import com.google.firebase.auth.FirebaseUser;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
 
-    private Button button;
-    private TextView txtHello,ISPView,LocView;
+    private Button btnLogout;
+    private Button btnWriteReview;
+    private Button btnUpdateReview;
+    private Button btnSearchNetwork;
+    private Button btnFeedbackForReview;
+
+    private TextView txtHello, ISPView, LocView;
     private User currentUser;
     public FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
@@ -37,53 +42,86 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         firebaseAuth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.btnLogout);
-        txtHello=findViewById(R.id.txtHello);
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnWriteReview = findViewById(R.id.btnWriteReview);
+        btnUpdateReview = findViewById(R.id.btnUpdateReview);
+        btnSearchNetwork = findViewById(R.id.btnSearchNetwork);
+        btnFeedbackForReview = findViewById(R.id.btnFeedbackForReview);
+
+        btnLogout.setOnClickListener(this);
+        btnWriteReview.setOnClickListener(this);
+        btnUpdateReview.setOnClickListener(this);
+        btnSearchNetwork.setOnClickListener(this);
+        btnFeedbackForReview.setOnClickListener(this);
+
+        txtHello = findViewById(R.id.txtHello);
         ISPView = findViewById(R.id.ISP);
         currentUser = SaveSharedPreferences.getUser(MainActivity2.this);
 
-        String[] names=currentUser.getUserName().split(" ");
+        String[] names = currentUser.getUserName().split(" ");
 
         txtHello.setText("Welcome " + names[0]);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+//        btnLogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                firebaseAuth.signOut();
+//                SaveSharedPreferences.clearUser(MainActivity2.this);
+//                startActivity(new Intent(MainActivity2.this,MainActivity.class));
+//            }
+//        });
+
+//        fetchIPInfo();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnWriteReview:
+                Toast.makeText(this, "Entering Isp-Ratings", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity2.this, IspRatingsActivity.class));
+                this.finish();
+                break;
+
+            case R.id.btnLogout:
                 firebaseAuth.signOut();
                 SaveSharedPreferences.clearUser(MainActivity2.this);
-                startActivity(new Intent(MainActivity2.this,MainActivity.class));
-            }
-        });
-
-        fetchIPInfo();
-    }
-    public void fetchIPInfo(){
-        progressDialog = new ProgressDialog(MainActivity2.this);
-        progressDialog.setMessage("Getting your Network Info..."); // show progess dialog till server responds
-        progressDialog.show();
-        GetDataService serviceIP= RetrofitClientInstance.getRetrofitInstance(this,0)
-                .create(GetDataService.class);
-        Call<IP> call = serviceIP.getIP();
-        call.enqueue(new Callback<IP>() {
-            @Override
-            public void onResponse(Call<IP> call, Response<IP> response) {
-                progressDialog.dismiss();
-                try {
-                    IP ip = response.body();
-                    ISPView.setText(ip.getIP());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    ISPView.setText("Json Error");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<IP> call, Throwable t) {
-                progressDialog.dismiss();
-                Toast.makeText(MainActivity2.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-                Log.i("Error",t.getMessage());
-            }
-        });
+                startActivity(new Intent(MainActivity2.this, MainActivity.class));
+                this.finish();
+                break;
+        }
     }
 }
+
+
+//    public void fetchIPInfo(){
+//        progressDialog = new ProgressDialog(MainActivity2.this);
+//        progressDialog.setMessage("Getting your Network Info..."); // show progess dialog till server responds
+//        progressDialog.show();
+//        GetDataService serviceIP= RetrofitClientInstance.getRetrofitInstance(this,0)
+//                .create(GetDataService.class);
+//        Call<IP> call = serviceIP.getIP();
+//        call.enqueue(new Callback<IP>() {
+//            @Override
+//            public void onResponse(Call<IP> call, Response<IP> response) {
+//                progressDialog.dismiss();
+//                try {
+//                    IP ip = response.body();
+//                    ISPView.setText(ip.getIP());
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                    ISPView.setText("Json Error");
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<IP> call, Throwable t) {
+//                progressDialog.dismiss();
+//                Toast.makeText(MainActivity2.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+//                Log.i("Error",t.getMessage());
+//            }
+//        });
+//    }
+//}
 //Use JsonReader.setLenient(true) to accept malformed JSON at line 1 column 1 path $
